bb9e49a9e7f6af0f9dba1734080125062b51adb3
mohamed Masstour <masstour01@gmail.com>
2022-09-07 18:27:09 +0100

    norm render wall file

--- source/render_wall.c
+++ source/render_wall.c
@@ -12,42 +12,38 @@
 
 #include "../includes/cub3d.h"
 
-void    draw_wall(t_data *data, t_rays *ray, int x, int y, int wall_strip_height)
+void	draw_wall(t_data *data, t_rays *ray, int x, int y)
 {
-	int jndex;
-	
-	jndex = (y + wall_strip_height); 
+	int	jndex;
+	int	color;
+	int	y_end_point;
+
+	y_end_point = ray->wall_strip_height;
+	if (y_end_point > H)
+		y_end_point = H;
+	jndex = (y + y_end_point);
 	while (y <= jndex)
 	{
-		// printf("y == %f, %f\n", y, x);
-		my_mlx_pixel_put(x, y, data, get_texture_color(ray, data, y, wall_strip_height));
+		color = get_texture_color(ray, data, y, y_end_point);
+		my_mlx_pixel_put(x, y, data, color);
 		y++;
 	}
 }
 
 void	rendering_walll(t_data *data, t_rays *rays, int ray_id)
 {
-	double ray_distance;
-	double distance_project_plane;
-	// double wall_strip_height;
-	int y_end_point;
+	double	ray_distance;
+	double	distance_project_plane;
+	int		y_start_point;
 
-	// if (!rays->hit)
-	// 	return; 
-	ray_distance = rays->distance * cos(rays->angle - data->player->rotation_angle);
+	ray_distance = rays->distance * cos(\
+	rays->angle - data->player->rotation_angle);
 	distance_project_plane = ((W / 2) / tan(FOV / 2));
-	rays->wall_strip_height = ((data->unit / ray_distance) * distance_project_plane);
-	data->player->y_start_point = (H / 2) - (rays->wall_strip_height / 2);
-	if (data->player->y_start_point < 0)
-		data->player->y_start_point = 0;
-	y_end_point = rays->wall_strip_height;
-	if (y_end_point > H)
-		y_end_point = H;
-	draw_wall(
-		data,
-		rays,
-		ray_id,
-		data->player->y_start_point,
-		y_end_point
-	);
+	rays->wall_strip_height = \
+	((data->unit / ray_distance) * distance_project_plane);
+	y_start_point = (H / 2) - (rays->wall_strip_height / 2);
+	if (y_start_point < 0)
+		y_start_point = 0;
+	draw_wall(\
+	data, rays, ray_id, y_start_point);
 }
